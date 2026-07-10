3f229a4859e3fd62b40003809d89cd75100c8d0a
1 BPM RO <1cia.rondon@gmail.com>
2026-07-10 08:07:13 -0400

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,1 +1,87 @@
+// Renderiza o esqueleto de cada equipe (Melhoria 5)
+function renderizarEquipe(militares, elementId, tipoServico) {
+
+    const container = document.getElementById(elementId);
+
+    if (!container) return;
+
+    container.innerHTML = "";
+
+    container.dataset.servico = tipoServico;
+    container.dataset.contId = elementId
+        .replace("dados-", "cont-")
+        .replace("guarda-", "guarda-");
+
+    const fragment = document.createDocumentFragment();
+
+    militares.forEach(militar => {
+
+    console.log(militar.texto);
+
+    const linha = document.createElement("div");
+
+        linha.className = "linha-militar";
+
+        linha.dataset.militarBruto = militar.texto;
+        linha.dataset.prontidaoInicial = militar.prontidao;
+
+        if (militar.prontidao === "INDISPONÍVEL") {
+            linha.classList.add("militar-indisponivel");
+        }
+
+       linha.innerHTML = `
+    <div class="militar-identidade">
+        ${formatarNomeMilitar(militar.texto)}
+    </div>
+
+    <div class="bloco-observacoes">
+        ${montarTags(militar.afastamentos, militar.observacoes)}
+    </div>
+`;
+
+        fragment.appendChild(linha);
+
+    });
+
+    container.appendChild(fragment);
+
+}
+
+function renderizarListaMenuAdmin() {
+    const div = document.getElementById("lista-afastados-atual");
+    if(!div) return;
+    
+    if(dadosGlobaisAfastados.length === 0) { 
+        div.innerHTML = "<em>Ninguém afastado hoje</em>"; 
+        return; 
+    }
+    
+    div.innerHTML = "";
+    dadosGlobaisAfastados.forEach(a => {
+        div.innerHTML += `
+            <div class="item-lista-afastado">
+                <div><strong>${a.nome}</strong> (${a.tipo})<br><small>Retorno: ${a.retorno}</small></div>
+                <button class="btn-dar-pronto" onclick="darProntoMilitar(${a.linha})">Pronto</button>
+            </div>
+        `;
+    });
+}
+
+// Atualizador Matemático de Prontos por Coluna
+function atualizarContadoresIndividuais(containerId, contadorId) {
+    const container = document.getElementById(containerId);
+    const painelContador = document.getElementById(contadorId);
+    if (!container || !painelContador) return;
+
+    const totalMilitares = container.querySelectorAll(".linha-militar");
+    let prontos = 0;
+    let indisp = 0;
+
+    totalMilitares.forEach(el => {
+        if (el.getAttribute("data-prontidao-inicial") === "INDISPONÍVEL") indisp++;
+        else prontos++;
+    });
+
+    painelContador.innerText = `Prontos: ${prontos} | Indisp: ${indisp}`;
+}
 
