3ea39544e1c29eb15fd826789a22fdcc939e3389
1 BPM RO <1cia.rondon@gmail.com>
2026-07-10 11:42:37 -0400

    Update dragdrop.js

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -1,1 +1,4 @@
-
+// ==========================================================================
+// CENTRAL DE MOVIMENTAÇÃO TÁTICA (DRAG & DROP ESPAÇO RESERVADO)
+// ==========================================================================
+console.log("✈️ Módulo dragdrop.js carregado e pronto para o combate.");
