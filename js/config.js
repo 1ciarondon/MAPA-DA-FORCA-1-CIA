9575fd0c87cc97a6f166820bc47aa63619dcb838
1 BPM RO <1cia.rondon@gmail.com>
2026-07-10 09:43:13 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+
+    API_URL: "https://script.google.com/macros/s/AKfycby5LqbrVC1udTsWiQxoay2Igda3NFFpbomBJ8d_-FjGPS6H5J42BrIQxsSwzVUZruCD/exec", 
+
+    EQUIPES: ["A", "B", "C", "D", "E"],
+
+    COLUNAS: {
+        A: 1,
+        B: 8,
+        C: 15,
+        D: 22,
+        E: 29
+    },
+
+    LIMITES: {
+        RADIOS: 19,
+        GUARDA: 6
+    }
+
+};
