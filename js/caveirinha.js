0053db38c1a1a52c44139662e00918a3ecb37bec
1 BPM RO <1cia.rondon@gmail.com>
2026-07-21 12:40:22 -0400

    Update caveirinha.js

--- js/caveirinha.js
+++ js/caveirinha.js
@@ -56,29 +56,91 @@ function fecharModalCaveirinha(){
 
 
 
-// Temporário até ligar na API
-
-function carregarCaveirinhas(){
+async function carregarCaveirinhas(){
 
     const lista =
-        document.getElementById(
-            "lista-caveirinhas-atual"
-        );
-
+        document.getElementById("lista-caveirinhas-atual");
 
     if(!lista) return;
 
-
     lista.innerHTML =
-    `
-        <div style="
-            padding:10px;
-            color:#777;
-            font-size:13px;
-            text-align:center;
-        ">
-            Nenhum débito carregado.
-        </div>
-    `;
+        "<div class='carregando'>Carregando...</div>";
+
+    try{
+
+        const resposta =
+            await buscarDadosAPI({
+                acao:"LISTAR_CAVEIRINHAS"
+            });
+
+        renderizarListaCaveirinhas(resposta);
+
+    }catch(e){
+
+        lista.innerHTML =
+            "<div class='erro'>Erro ao carregar.</div>";
+
+        console.error(e);
+
+    }
+
+}
+
+function renderizarListaCaveirinhas(lista){
+
+    const container =
+        document.getElementById("lista-caveirinhas-atual");
+
+    if(!container) return;
+
+    if(!lista.length){
+
+        container.innerHTML =
+            "<div class='vazio'>Nenhum débito.</div>";
+
+        return;
+
+    }
+
+    container.innerHTML = "";
+
+    lista.forEach(item=>{
+
+        const card=document.createElement("div");
+
+        card.className="item-caveirinha";
+
+        card.innerHTML=`
+
+            <strong>${item.nome}</strong>
+
+            <div>${item.motivo}</div>
+
+            <small>
+
+                Folga:
+                ${item.dataFolga}
+
+            </small>
+
+            <small>
+
+                Pagamento:
+                ${item.dataPagamento}
+
+            </small>
+
+            <button
+                onclick="quitarDebitoCaveirinha(${item.linha})">
+
+                ✔ Quitar
+
+            </button>
+
+        `;
+
+        container.appendChild(card);
+
+    });
 
 }
